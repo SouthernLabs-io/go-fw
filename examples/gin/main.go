132de7a27188cbc911581966bb55695433fa7306
Simon Paredes <paredess@gmail.com>
2024-05-08 18:22:16 -0400

    cleanup

--- examples/gin/main.go
+++ examples/gin/main.go
@@ -4,13 +4,10 @@ import (
 	"go.uber.org/fx"
 
 	"github.com/southernlabs-io/go-fw/bootstrap"
-	"github.com/southernlabs-io/go-fw/config/aws"
 	"github.com/southernlabs-io/go-fw/errors"
 	"github.com/southernlabs-io/go-fw/panics"
 	"github.com/southernlabs-io/go-fw/rest"
 	"github.com/southernlabs-io/go-fw/rest/middleware"
-	"github.com/southernlabs-io/go-fw/secrets"
-	"github.com/southernlabs-io/go-fw/slack"
 )
 
 func main() {
@@ -20,18 +17,10 @@ func main() {
 		// Service configuration
 		fx.Provide(NewConfig),
 
-		// AWS Config
-		fx.Provide(aws.NewAWSConfig),
-
-		// Secrets with AWS
-		secrets.ModuleAWS,
-
 		// Middlewares
 		middleware.RequestLoggerModule,
 
-		// Use Slack notifications
-		slack.Module,
-
+		// Register Resources
 		rest.ProvideAsResource(NewMyResource),
 	)
 	err := bootstrap.NewAppWithServe(deps).Execute()
