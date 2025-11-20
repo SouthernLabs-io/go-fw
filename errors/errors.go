659bc4b5d44b04db71e66a4908f0957b3003d8de
Simon Paredes <paredess@gmail.com>
2025-11-20 18:34:52 -0300

    quick error

--- errors/errors.go
+++ errors/errors.go
@@ -339,6 +339,12 @@ func NewNotFoundf(format string, args ...any) *Error {
 	return Newf(ErrCodeNotFound, format, args...)
 }
 
+// NewNotAllowedf creates a new error with the ErrCodeNotAllowed code and the given message format/args.
+// This errors should be mapped to HTTP 403.
+func NewNotAllowedf(format string, args ...any) *Error {
+	return Newf(ErrCodeNotAllowed, format, args...)
+}
+
 //#region copy from errors.go
 
 // ErrUnsupported is a copy of the errors.ErrUnsupported variable from the go std core.
