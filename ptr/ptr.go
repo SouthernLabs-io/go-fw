47545a5357e6da55f127177e350f418f523a9eb5
Simon Paredes <paredess@gmail.com>
2025-12-17 20:15:47 -0300

    more toPtr variants

--- ptr/ptr.go
+++ ptr/ptr.go
@@ -1,6 +1,9 @@
 package ptr
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 // FormatPtr will format the value pointed by p using "%v". It will format it as
 // "<nil>" if p is nil.
@@ -16,6 +19,23 @@ func ToPtr[T any](v T) *T {
 	return &v
 }
 
+// ToPtrIfNotNil returns a pointer to v if v is not nil. Otherwise, it returns nil. It is reflection based so prefer to use ToPtrIfNotZero for all comparable types.
+func ToPtrIfNotNil[T any](v T) *T {
+	t := reflect.TypeOf(v)
+	if t == nil {
+		return nil
+	}
+	kind := t.Kind()
+	if kind == reflect.Slice || kind == reflect.Map || kind == reflect.Chan || kind == reflect.Func {
+		val := reflect.ValueOf(v)
+		if val.IsNil() {
+			return nil
+		}
+	}
+	return &v
+}
+
+// ToPtrIfNotZero returns a pointer to v if v is not the zero value for its type.
 func ToPtrIfNotZero[T comparable](v T) *T {
 	var zero T
 	if v == zero {
@@ -25,7 +45,7 @@ func ToPtrIfNotZero[T comparable](v T) *T {
 }
 
 // ToValue returns the value referenced by p. Returns a zero value if p is nil.
-func ToValue[T comparable](p *T) (v T) {
+func ToValue[T any](p *T) (v T) {
 	if p == nil {
 		return v
 	}
--- ptr/ptr_test.go
+++ ptr/ptr_test.go
@@ -19,6 +19,54 @@ func TestToPtr(t *testing.T) {
 
 	var intVal = 123
 	require.Equal(t, &intVal, ToPtr(intVal))
+
+	var m map[string]int
+	require.Equal(t, map[string]int(nil), m)
+	require.NotNil(t, ToPtr(m))
+}
+
+func TestToPtrIfNotNil(t *testing.T) {
+	var m map[string]int
+	require.Nil(t, ToPtrIfNotNil(m))
+
+	m = map[string]int{"a": 1}
+	ptr := ToPtrIfNotNil(m)
+	require.NotNil(t, ptr)
+	require.Equal(t, m, *ptr)
+
+	var ch chan int
+	require.Nil(t, ToPtrIfNotNil(ch))
+
+	ch = make(chan int)
+	ptrCh := ToPtrIfNotNil(ch)
+	require.NotNil(t, ptrCh)
+	require.Equal(t, ch, *ptrCh)
+}
+
+func TestToPtrIfNotZero(t *testing.T) {
+	var str string
+	require.Nil(t, ToPtrIfNotZero(str))
+
+	str = "hi"
+	ptr := ToPtrIfNotZero(str)
+	require.NotNil(t, ptr)
+	require.Equal(t, str, *ptr)
+
+	var intVal int
+	require.Nil(t, ToPtrIfNotZero(intVal))
+
+	intVal = 123
+	ptrInt := ToPtrIfNotZero(intVal)
+	require.NotNil(t, ptrInt)
+	require.Equal(t, intVal, *ptrInt)
+
+	var structVal struct{ field string }
+	require.Nil(t, ToPtrIfNotZero(structVal))
+
+	structVal = struct{ field string }{field: "value"}
+	ptrStruct := ToPtrIfNotZero(structVal)
+	require.NotNil(t, ptrStruct)
+	require.Equal(t, structVal, *ptrStruct)
 }
 
 func TestToValue(t *testing.T) {
