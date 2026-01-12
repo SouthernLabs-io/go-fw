7a74fcd28ef580a69ac0c51bd7835268b37bd0b8
Simon Paredes <paredess@gmail.com>
2026-01-12 14:29:16 -0300

    Value and Key slices for map

--- functional/maps/maps.go
+++ functional/maps/maps.go
@@ -53,3 +53,23 @@ func FromEntries[K comparable, V any](entries []Entry[K, V]) map[K]V {
 	}
 	return m
 }
+
+func ValuesSlice[K comparable, V any](m map[K]V) []V {
+	values := make([]V, len(m))
+	i := 0
+	for _, v := range m {
+		values[i] = v
+		i++
+	}
+	return values
+}
+
+func KeysSlice[K comparable, V any](m map[K]V) []K {
+	keys := make([]K, len(m))
+	i := 0
+	for k := range m {
+		keys[i] = k
+		i++
+	}
+	return keys
+}
